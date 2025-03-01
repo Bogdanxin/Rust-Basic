e25d53cd333b1455aaa185b8c36e79849ac6b9db
BogdanXin <gwx20000117@gmail.com>
2025-03-01 21:54:14 +0800

    all finish

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -5,24 +5,25 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let data = "Rust is great!".to_string();
-
-    get_char(data);
-
-    string_uppercase(&data);
+    let mut data = "Rust is great!".to_string();
+    // 因为下一个函数也要对 data 做借用，
+    // 所以 get_char 方法不能做 move 操作，只能使用借用的方式
+    get_char(&data);
+    // 因为函数要对 data 做改动，所以需要传入参数以及函数声明都要设置为 mut
+    string_uppercase(data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+// 如果是借用的话，对应的函数也要有借用声明，rust 在后面会自己解引用
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+// 因为要获取 data 所有权，所以data 属性声明不能是引用&
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
 
     println!("{}", data);
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -32,11 +31,20 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input:Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Trim => output.push(string.trim_start().trim_end().to_string()),
+                Command::Uppercase => output.push(string.to_uppercase().to_string()),
+                Command::Append(size) =>{
+                    let mut tmp = string.clone();
+                    for i in 0..*size {
+                        tmp.push_str("bar");
+                    }
+                    output.push(tmp);
+                },
+            }
         }
         output
     }
@@ -44,8 +52,8 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    // TODO: What do we need to import to have `transformer` in scope??
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
